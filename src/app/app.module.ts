6541e1b73d622575c5feba6dc11567e68e0d4d2e
Tony Stevanovich <tonys@yahoo.com>
2018-05-10 13:20:06 -0400

    components and bootstrap setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from '@app/app.component';
+import { NavbarComponent } from '@app/components/navbar/navbar.component';
+import { LogFormComponent } from '@app/components/log-form/log-form.component';
+import { LogsComponent } from '@app/components/logs/logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogFormComponent,
+    LogsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
