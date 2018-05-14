dd5850d71badc1861ac58f3f4c3de5cab967d958
Tony Stevanovich <tonys@yahoo.com>
2018-05-14 10:24:54 -0400

    add uuid, crud logs

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 
 import { Log } from '@app/models/Log';
 import { LogService } from '@app/services/log.service';
@@ -12,6 +13,7 @@ export class LogFormComponent implements OnInit {
   id: string;
   text: string;
   date: any;
+  isNew = true;
 
   constructor(private logService: LogService) {}
 
@@ -19,10 +21,34 @@ export class LogFormComponent implements OnInit {
     // Subscribe to the selectedLog observable
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
     });
+  }
+
+  onSubmit() {
+    // Check if new log
+    if (this.isNew) {
+      // Create a new log
+      const newLog = {
+        id: uuid(),
+        text: this.text,
+        date: new Date()
+      };
+      // Add log
+      this.logService.addLog(newLog);
+    } else {
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      // Edit Log
+      this.logService.updateLog(updLog);
+    }
   }
 }
